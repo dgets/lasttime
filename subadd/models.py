0e01667bacfd308189973b2acf0f1658df5ad990
Damon Getsman <damo.gets@gmail.com>
2018-09-10 19:22:55 -0500

    Adding model & view for subadd.

--- subadd/models.py
+++ subadd/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Substance(models.Model):
+    common_name = models.CharField(max_length=40)
+    sci_name = models.CharField(max_length=60)
+    half_life = models.IntegerField()
+
+    def __str__(self):
+        return common_name + " (" + sci_name + "): half-life: " + half_life.str()
+
+
--- subadd/views.py
+++ subadd/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def subadd(request):
+    all_subs = Substance.objects.all()
+    context = {
+        'all_subs': all_subs,
+    }
+
+    return render(request, 'subadd/index.html', context)
+
+
