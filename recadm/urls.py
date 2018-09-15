1d828225104e40664dd3acec90e64579c4a84e29
Damon Getsman <damo.gets@gmail.com>
2018-09-15 13:28:18 -0500

    Worked on an index view; need to be able to add records next, in order to test it out.

--- recadm/urls.py
+++ recadm/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'viewdox'
 urlpatterns = [
     path('<int:topic_id>/', views.detail, name='detail'),
+    path('', views.index, name='index'),
     path('add', views.add, name='add'),
     path('add_new', views.add_new, name='add_new'),
 ]
