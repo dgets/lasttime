028e743e123aab979516ecc881d6935b278259f3
Damon Getsman <damo.gets@gmail.com>
2018-09-18 04:14:43 -0500

    May have solved #6; we'll know after add administration functionality is completed here.  Tried building a custom set of records in views.py instead of cross referencing in template logic.  MVC separation, amirite?

--- recadm/views.py
+++ recadm/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Usage
+from subadd.models import Substance
 
 # Create your views here.
 
 def index(request):
     recent_administrations = Usage.objects.all()
+    substances = Substance.objects.all()
+    mydata = [ ] #this can no doubt be handled better via the db
+
+    for administration in recent_administrations:
+        #we only need admin timestamp, dosage, and substance name
+        mydata.add({'ts': administration.timestamp,
+            'id': administration.id,
+            'dosage': administration.dosage,
+            'substance_name': substances.object.get(pk=administration.sub),})
+
     context = {
-        'recent_administrations': recent_administrations,
+        #'substances': substances,
+        #'recent_administrations': recent_administrations,
+        'mydata': mydata,
     }
 
     return render(request, 'recadm/index.html', context)
