52f9865c356af700913e9c65aa08b136a317797e
Damon Getsman <damo.gets@gmail.com>
2018-12-30 17:43:58 -0600

    Closed #32; don't forget app_name = blah in urls.py ever again (&).

--- dataview/urls.py
+++ dataview/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'dataview'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.SubAdminDataView.as_view(), name='data_summary')
