fd40efa01e5a728be7b7ec7c3e16f7b5d343d545
Damon Getsman <damo.gets@gmail.com>
2019-01-20 07:32:21 -0600

    Online documentation is working with a rudimentary set of functionality; closed #58.

--- dox/views.py
+++ dox/views.py
@@ -5,8 +5,10 @@ from django.core.paginator import Paginator
 from .models import MainHelpTopic, MainHelpTopicDetail, SpecificViewHelpTopic, SpecificViewTopicDetail
 from home.models import HeaderInfo, NavInfo
 
+# just put two and two together here and realized that I really don't want
+# these particular views restricted to just logged in users; derp!
 
-@login_required
+
 def index(request):
     """
     View displays the topics links for everything available, paginated if
@@ -25,17 +27,21 @@ def index(request):
     return render(request, 'dox/index.html', add_header_info({'topics': topics_set,}))
 
 
-@login_required
-def detail(request):
+def detail(request, topic_id):
     """
     Displays the detailed information for whatever link was selected in the
     index view, above.
 
     :param request:
+    :param topic_id:
     :return:
     """
 
+    topic_info = {}
+    topic_info['primary'] = MainHelpTopic.objects.get(id=topic_id)
+    topic_info['supporting'] = MainHelpTopicDetail.objects.filter(topic=topic_id)
 
+    return render(request, 'dox/detail.html', add_header_info(topic_info))
 
 
 def add_header_info(page_data):
@@ -44,7 +50,7 @@ def add_header_info(page_data):
     adds the 'NavInfo' and 'HeaderInfo' keys to it, pointing to the
     applicable data for the header & footer schitt.
 
-    :param previous_context:
+    :param page_data: previous context for the page sans header stuff
     :return: new context (dict)
     """
 
