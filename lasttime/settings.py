6dfbcae9368c64ad96e062223db5465709cfd8ea
Damon Getsman <damo.gets@gmail.com>
2019-01-26 08:08:17 -0600

    Switched back to postgres due to sqlite horkage.

--- lasttime/settings.py
+++ lasttime/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'lasttime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lasttime',
+        'USER': 'damo',
+        'PASSWORD': 'huggaTree',
+        'HOST': '127.0.0.1'
     }
 }
 
